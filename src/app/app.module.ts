ab2acb5eb0c65faa71754186e77b792e75033ee8
sureshvijayan07 <sureshvijayan29592@gmail.com>
2024-01-08 15:17:01 +0000

    Fixed Responisve Issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SiteListComponent } from './site-list/site-list.component';
+import { PasswordListComponent } from './password-list/password-list.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SiteListComponent,
+    PasswordListComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'mag-pm-app-d48db',
+        appId: '1:12335752275:web:7e5fbcdc4846a14fe6d2a6',
+        storageBucket: 'mag-pm-app-d48db.appspot.com',
+        apiKey: 'AIzaSyCkNj2u_MEqmSn6FIympB82c3dCg2OJr-Q',
+        authDomain: 'mag-pm-app-d48db.firebaseapp.com',
+        messagingSenderId: '12335752275',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { PasswordManagerService } from '../password-manager.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  isError: boolean = false;
+  constructor(
+    private passwordManagerService: PasswordManagerService,
+    private router: Router
+  ) {}
+  onSubmit(values: any) {
+    this.passwordManagerService
+      .login(values.email, values.password)
+      .then(() => {
+        this.router.navigate(['/site-list']);
+      })
+      .catch((err) => {
+        this.isError = true;
+      });
+  }
+}
